9feccab8b07a36cdb8ffc176146719490da18b6c
Your Name <you@example.com>
2019-05-21 16:11:55 +0800

    k/ma10/ma30/volume/kd

--- day25.py
+++ day25.py
@@ -19,3 +19,47 @@ import datetime as datetime
 
 #talib
 import talib
+
+
+
+
+start = datetime.datetime(2018,10,1)
+df_2330 = pdr.DataReader('2330.TW', 'yahoo', start=start)
+print(df_2330)
+
+df_2330.index = df_2330.index.format(formatter=lambda x: x.strftime('%Y-%m-%d')) 
+
+
+
+sma_10 = talib.SMA(np.array(df_2330['Close']), 10)
+sma_30 = talib.SMA(np.array(df_2330['Close']), 30)
+df_2330['k'], df_2330['d'] = talib.STOCH(df_2330['High'], df_2330['Low'], df_2330['Close'])
+df_2330['k'].fillna(value=0, inplace=True)
+df_2330['d'].fillna(value=0, inplace=True)
+
+fig = plt.figure(figsize=(24, 20))
+ax = fig.add_axes([0,0.3,1,0.4])
+ax2 = fig.add_axes([0,0.2,1,0.1])
+ax3 = fig.add_axes([0,0,1,0.2])
+
+ax.set_xticks(range(0, len(df_2330.index), 10))
+ax.set_xticklabels(df_2330.index[::10])
+mpf.candlestick2_ochl(ax, df_2330['Open'], df_2330['Close'], df_2330['High'],
+                              df_2330['Low'], width=0.6, colorup='r', colordown='g', alpha=0.75)
+plt.rcParams['font.sans-serif']=['Microsoft JhengHei'] 
+ax.plot(sma_10, label='10日均線')
+ax.plot(sma_30, label='30日均線')
+
+ax2.plot(df_2330['k'], label='K值')
+ax2.plot(df_2330['d'], label='D值')
+ax2.set_xticks(range(0, len(df_2330.index), 10))
+ax2.set_xticklabels(df_2330.index[::10])
+
+mpf.volume_overlay(ax3, df_2330['Open'], df_2330['Close'], df_2330['Volume'], colorup='r', colordown='g', width=0.5, alpha=0.8)
+ax3.set_xticks(range(0, len(df_2330.index), 10))
+ax3.set_xticklabels(df_2330.index[::10])
+
+ax.legend();
+ax2.legend();
+
+plt.savefig("day25.png")
