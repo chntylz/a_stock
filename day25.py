d57555b38b9c09b832534d0e8774dc3e58ec31ea
Your Name <you@example.com>
2019-05-21 14:11:12 +0800

    base

---
+++ day25.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# basic
+import numpy as np
+import pandas as pd
+
+# get data
+import pandas_datareader as pdr
+
+# visual
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+#%matplotlib inline
+import seaborn as sns
+
+#time
+import datetime as datetime
+
+#talib
+import talib
