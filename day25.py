3b9a489817756a585af8642bb190cc3b6ff6fd9c
Your Name <you@example.com>
2019-05-22 13:32:52 +0800

    1. add macd transaction algorithm. 2.add boll function

--- day25.py
+++ day25.py
@@ -24,8 +24,8 @@ warnings.simplefilter(action = "ignore", category = RuntimeWarning)
 start = datetime.datetime(2018,10,1)
 df_2330 = pdr.DataReader('2330.TW', 'yahoo', start=start)
 #print(df_2330)
-df_2330.index = df_2330.index.format(formatter=lambda x: x.strftime('%Y-%m-%d')) 
-
+df_2330.index = df_2330.index.format(formatter=lambda x: x.strftime('%Y-%m-%d'))
+#print(df_2330.index[2])
 sma_5 = talib.SMA(np.array(df_2330['Close']), 5)
 sma_13 = talib.SMA(np.array(df_2330['Close']), 13)
 sma_21 = talib.SMA(np.array(df_2330['Close']), 21)
@@ -43,14 +43,49 @@ df_2330['MACD'],df_2330['MACDsignal'],df_2330['MACDhist'] = talib.MACD(np.array(
 dif, dea, macd_hist = talib.MACD(np.array(df_2330['Close']), fastperiod=12, slowperiod=26, signalperiod=9)
 
 
+
+# 程序交易 （K线图数据，分钟/）
+# 使用程序的判断依据来模拟MACD指标交易情况，买入、卖出
+# 以200天为例，从第一天到第200天每天进行判断
+for i in range(1, dif.size):
+
+    # 进行交易判断
+    # DIF差离值和DEM讯号线的交替状态
+
+    # 考虑买入的信号
+    # 买入：金叉信号：昨天：DIF差离值 < DEA讯号线 and DIF差离值 > DEA讯号线 
+    if dif[i-1] - dea[i-1] < 0 and dif[i] - dea[i] > 0:
+        print("在第%d天:%s：买入了某某股票多少量的股票:%d" % (i, df_2330.index[i],df_2330['Close'][i]))
+
+    # 考虑卖出的信号
+    # 买入：死叉信号：昨天：DIF差离值 > DEA讯号线 and DIF差离值 <  DEA讯号线
+    if dif[i-1] - dea[i-1] > 0 and dif[i] - dea[i] < 0:
+        print("在第%d天:%s：卖出了某某股票多少量的股票:%d" %  (i, df_2330.index[i],df_2330['Close'][i]))
+
+
+
+
 plt.style.use('bmh')
 fig = plt.figure(figsize=(24, 20))
+
+ax0  = fig.add_axes([0, 0.8, 1, 0.4])
+ax0.grid()
 ax  = fig.add_axes([0, 0.4, 1, 0.4])
-ax.grid()
 ax2 = fig.add_axes([0, 0.3, 1, 0.1])
 ax3 = fig.add_axes([0, 0.2, 1, 0.1])
 ax4 = fig.add_axes([0, 0,   1, 0.2])
 
+
+#candles
+ax0.set_xticks(range(0, len(df_2330.index), 10))
+ax0.set_xticklabels(df_2330.index[::10])
+mpf.candlestick2_ochl(ax0, df_2330['Open'], df_2330['Close'], df_2330['High'],
+                              df_2330['Low'], width=0.6, colorup='r', colordown='g', alpha=0.75)
+#boll
+upperband, middleband, lowerband = talib.BBANDS(np.array(df_2330['Close']),timeperiod=20, nbdevdn=2, nbdevup=2)
+ax0.plot( upperband, label="上轨线")
+ax0.plot( middleband, label="中轨线")
+ax0.plot( lowerband, label="下轨线")
 
 #candles
 ax.set_xticks(range(0, len(df_2330.index), 10))
