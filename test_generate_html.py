ddc306f6768a4425e7defb87efc6f21df716a9b9
Your Name <you@example.com>
2019-07-12 09:35:41 +0800

    add continuous func

--- test_generate_html.py
+++ test_generate_html.py
@@ -11,26 +11,35 @@ sdata=HData_select("usr","usr")
 
 nowdate=datetime.datetime.now().date()
 #nowdate=nowdate-datetime.timedelta(1)
-src_dir=nowdate.strftime("%Y-%m-%d")
+lastdate=nowdate-datetime.timedelta(1)
+
+curr_day=nowdate.strftime("%Y-%m-%d")
+last_day=lastdate.strftime("%Y-%m-%d")
+
 
 def showImageInHTML(imageTypes,savedir):
-    files=getAllFiles(savedir+'/' + src_dir)
+    files=getAllFiles(savedir+'/' + curr_day)
     #print("file:%s" % (files))
     images=[f for f in files if f[f.rfind('.')+1:] in imageTypes]
     #print("%s"%(images))
     images=[item for item in images if os.path.getsize(item)>5*1024]
     #print("%s"%(images))
-    #images=[src_dir+item[item.rfind('/'):] for item in images]
+    #images=[curr_day+item[item.rfind('/'):] for item in images]
     images=[item[item.rfind('/')+1:] for item in images]
     print("%s"%(images))
-    newfile='%s/%s'%(savedir, src_dir + '/' + src_dir + '.html')
+    newfile='%s/%s'%(savedir, curr_day + '/' + curr_day + '.html')
+    
+    #get continuous stock_code
+    conti_df = get_continuous_item(last_day, curr_day)
+    conti_list = list(conti_df['stock_code'])
+    print("last_day:%s, curr_day:%s conti_list:%s" % (last_day, curr_day, conti_list))
     with open(newfile,'w') as f:
 
         f.write('<!DOCTYPE html>\n')
         f.write('<html>\n')
         f.write('<head>\n')
         f.write('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n')
-        f.write('<title> %s </title>\n'%(src_dir))
+        f.write('<title> %s </title>\n'%(curr_day))
         f.write('\n')
         f.write('\n')
         f.write('<style type="text/css">a {text-decoration: none}</style>\n')
@@ -56,17 +65,20 @@ def showImageInHTML(imageTypes,savedir):
             
             print('%s' % (stock_code[0:2]))
             if stock_code[0:2] == '60':
-                stock_code='SH'+stock_code
+                stock_code_new='SH'+stock_code
             else:
-                stock_code='SZ'+stock_code
+                stock_code_new='SZ'+stock_code
 
-            print('%s' % (stock_code))
-            xueqiu_url='https://xueqiu.com/S/' + stock_code
+            print('%s' % (stock_code_new))
+            xueqiu_url='https://xueqiu.com/S/' + stock_code_new
 
             f.write('<p>\n')
-            f.write('<a href="%s" target="_blank"> %s </a>' % (image, tmp_image))
+            if stock_code in conti_list:
+                f.write('<a href="%s" target="_blank" style="color: #FF0000"> %s </a>' % (image, tmp_image))
+            else:
+                f.write('<a href="%s" target="_blank"> %s </a>' % (image, tmp_image))
             f.write('---->')
-            f.write('<a href="%s" target="_blank">(%s) </a>\n' % (xueqiu_url , 'xueqiu:' + stock_code))
+            f.write('<a href="%s" target="_blank">(%s) </a>\n' % (xueqiu_url , 'xueqiu:' + stock_code_new))
             f.write('</p>\n')
             
             
@@ -80,8 +92,8 @@ def showImageInHTML(imageTypes,savedir):
         f.write('</html>\n')
         f.write('\n')
     
-	
-    shell_cmd2='cp -rf ' + src_dir + ' /var/www/html/'
+    
+    shell_cmd2='cp -rf ' + curr_day + ' /var/www/html/'
     os.system(shell_cmd2)
     print ('success,images are wrapped up in %s' % (newfile))
 
@@ -110,19 +122,27 @@ def get_today_item(today):
     # print("today:%s: %s" % (today, df.head(100)))
     df = df[df.record_date==today]
     # print("%s" % (df.head(10)))
+    
+    return df
 
-    
-    
+
+def get_continuous_item(today, yesterday):
+    df=sdata.my2_get_continuous_hdata_of_stock(yesterday, today)
+    print("%s" % (df.head(10)))
     return df
 
         
 if __name__ == '__main__':
+    '''
     nowdate=datetime.datetime.now().date()
-    nowdate=nowdate-datetime.timedelta(1)
+    yesterday=nowdate-datetime.timedelta(1)
     
     today = nowdate.strftime("%Y-%m-%d")
-    df=get_today_item(today)
-
+    yesterday = yesterday.strftime("%Y-%m-%d")
+    today_df=get_today_item(today)
+    #continuous_df = get_continuous_item(yesterday, today)
+    continuous_df = get_continuous_item('2019-07-08', '2019-07-09')
+    '''
 
     savedir=cur_file_dir()#获取当前.py脚本文件的文件路径
     showImageInHTML(('jpg','png','gif'), savedir)#浏览所有jpg,png,gif文件
