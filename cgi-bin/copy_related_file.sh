cd8942d43bae39293f58703244fe76989fa408ff
Your Name <you@example.com>
2020-02-29 14:05:36 +0800

    add copy_related_file.sh for cgi-bin

---
+++ cgi-bin/copy_related_file.sh
@@ -0,0 +1,12 @@
+cp -rf ../file_interface.py .
+cp -rf ../HData_day.py      .
+cp -rf ../HData_hsgt.py     .
+cp -rf ../zig.py            .
+
+
+#rm    file_interface.py
+#rm    HData_day.py     
+#rm    HData_hsgt.py    
+#rm    zig.py           
+
+cp -rf * /var/www/cgi-bin/
