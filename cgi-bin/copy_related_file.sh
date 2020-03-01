1c7236be6e8beb224f72bce94eadb71497196b73
Your Name <you@example.com>
2020-03-01 22:48:46 +0800

    modify copy_related_file.sh

--- cgi-bin/copy_related_file.sh
+++ cgi-bin/copy_related_file.sh
@@ -2,12 +2,14 @@ cp -rf ../file_interface.py .
 cp -rf ../HData_day.py      .
 cp -rf ../HData_hsgt.py     .
 cp -rf ../zig.py            .
+cp -rf ../comm_generate_html.py .
 
 
 #rm    file_interface.py
 #rm    HData_day.py     
 #rm    HData_hsgt.py    
 #rm    zig.py           
+#rm    comm_generate_html.py
 
 cp -rf *.py /var/www/cgi-bin/
 cp -rf *.cgi /var/www/cgi-bin/
