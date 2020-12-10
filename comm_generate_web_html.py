bf9bb415ccd3a88a639ca522777ec04771eaae22
Your Name <you@example.com>
2020-12-10 10:53:08 +0800

    resort peach zig quad

--- comm_generate_web_html.py
+++ comm_generate_web_html.py
@@ -24,6 +24,7 @@ from Stocks import *
 from HData_hsgt import *
 from HData_dailybasic import *
 from HData_select import *
+from HData_day import *
 
 from file_interface import *
 
@@ -31,6 +32,7 @@ hsgtdata=HData_hsgt("usr","usr")
 stocks=Stocks("usr","usr")
 db_daily=HData_dailybasic("usr", "usr")
 sdata=HData_select("usr","usr")
+hdata_day=HData_day("usr","usr")
 
 token='21dddafc47513ea46b89057b2c4edf7b44882b3e92274b431f199552'
 pro = ts.pro_api(token)
@@ -140,11 +142,14 @@ def hsgt_get_continuous_info(df, select):
                 
         money_total = round(money_total,2)
         if debug:
-            print(max_date, stock_code, stock_cname, hk_pct, close, a_pct, is_zig, is_quad, is_peach, delta1, i, money_total)
+            print(max_date, stock_code, stock_cname, hk_pct, close, a_pct, \
+                    is_peach, is_zig, is_quad, delta1, i, money_total)
 
-        data_list.append([max_date, stock_code, stock_cname, hk_pct, close, a_pct, is_zig, is_quad, is_peach, delta1, delta1_m, i, money_total])  #i  is conti_day
+        data_list.append([max_date, stock_code, stock_cname, hk_pct, close, a_pct, \
+                is_peach, is_zig, is_quad, delta1, delta1_m, i, money_total])  #i  is conti_day
 
-    data_column=['record_date', 'stock_code', 'stock_cname', 'hk_pct', 'close', 'a_pct', 'is_zig', 'is_quad', 'is_peach', 'delta1', 'delta1_m', 'conti_day', 'money_total']
+    data_column=['record_date', 'stock_code', 'stock_cname', 'hk_pct', 'close', 'a_pct', \
+            'peach', 'zig', 'quad', 'delta1', 'delta1_m', 'conti_day', 'money_total']
 
     ret_df = pd.DataFrame(data_list, columns=data_column)
     ret_df['m_per_day'] = ret_df.money_total / ret_df.conti_day
@@ -227,7 +232,11 @@ def get_conti_day_from_s_db(image_name):
     while True:
         s_date = (now_date - datetime.timedelta(int(i))).strftime("%Y-%m-%d")
         e_date = (now_date - datetime.timedelta(int(i-1))).strftime("%Y-%m-%d")
-        select_df = sdata.get_data_from_hdata(stock_code=stock_code, start_date=s_date, end_date=e_date, limit=1)
+        select_df = sdata.get_data_from_hdata(stock_code=stock_code, \
+                start_date=s_date, \
+                end_date=e_date, \
+                limit=1)
+
         if debug:
             print('i = %d,s_date=%s, e_date=%s select_df:' % (i, e_date, s_date))
             print('%s' % select_df)
@@ -286,11 +295,14 @@ def comm_write_to_file(f, k, df, filename):
             if k is -1: # normal case
                 #data_column=['record_date', 'stock_code', 'stock_cname', 'hk_pct', 'close', 'delta1', 'delta1_m', 'conti_day', 'money_total']
                 if(j == 0): 
-                    f.write('           <a href="%s" target="_blank"> %s[fina]</a>\n'%(fina_url, element_value))
+                    f.write('           <a href="%s" target="_blank"> %s[fina]</a>\n'%\
+                            (fina_url, element_value))
                 elif(j == 1): 
-                    f.write('           <a href="%s" target="_blank"> %s[hsgt]</a>\n'%(hsgt_url, element_value))
+                    f.write('           <a href="%s" target="_blank"> %s[hsgt]</a>\n'%\
+                            (hsgt_url, element_value))
                 elif(j == 2):
-                    f.write('           <a href="%s" target="_blank"> %s</a>\n'%(xueqiu_url, element_value))
+                    f.write('           <a href="%s" target="_blank"> %s</a>\n'%\
+                            (xueqiu_url, element_value))
                 elif(j == 3):
                     f.write('           <a> %.2f%s</a>\n'%(element_value, '%'))
                 elif(j == 11):
@@ -462,7 +474,10 @@ def comm_handle_html_body(filename, all_df, select='topy10'):
             f.write('<p style="color: #FF0000"> delta1_m sum is: %.2fw rmb </p>\n'%(daily_net))
             if select is 'top10':
 
-                delta_list = ['hk_pct', 'delta1', 'delta1_m', 'delta2', 'delta3', 'delta4',  'delta5', 'delta10', 'delta21', 'delta120', 'delta2_m',    'delta3_m',   'delta4_m', 'delta5_m', 'delta10_m', 'delta21_m']
+                delta_list = ['hk_pct', 'delta1', 'delta1_m', 'delta2', 'delta3', 'delta4',  \
+                        'delta5', 'delta10', 'delta21', 'delta120', \
+                        'delta2_m', 'delta3_m', 'delta4_m', 'delta5_m', \
+                        'delta10_m', 'delta21_m']
                 lst_len = len(delta_list)
                 for k in range(0, lst_len):
                     f.write('           <p style="color: #FF0000">------------------------------------top10 order by %s desc---------------------------------------------- </p>\n'%(delta_list[k]))
@@ -665,8 +680,15 @@ def comm_generate_web_dataframe(curr_dir, images, curr_day, dict_industry):
         close_p = (C - pre_close)/pre_close
         close_p = round (close_p.value, 4) * 100
 
-        all_df = hsgtdata.get_data_from_hdata(stock_code=stock_code, end_date=curr_day, limit=60)
-        hsgt_date, hsgt_share, hsgt_percent, hsgt_delta1, hsgt_deltam, conti_day, money_total = comm_handle_hsgt_data(all_df)
+        hsgt_df = hsgtdata.get_data_from_hdata(stock_code=stock_code, end_date=curr_day, limit=60)
+        hsgt_date, hsgt_share, hsgt_percent, hsgt_delta1, hsgt_deltam, conti_day, money_total \
+                = comm_handle_hsgt_data(hsgt_df)
+        
+        daily_df = hdata_day.get_day_hdata_of_stock(curr_day)
+        is_zig=daily_df[daily_df['stock_code']==stock_code]['is_zig'][0]
+        is_quad=daily_df[daily_df['stock_code']==stock_code]['is_quad'][0]
+        is_peach=daily_df[daily_df['stock_code']==stock_code]['is_peach'][0]
+
 
         total_mv =  comm_get_total_mv(stock_code)
 
@@ -674,9 +696,16 @@ def comm_generate_web_dataframe(curr_dir, images, curr_day, dict_industry):
         insert_industry(dict_industry, industry_name)
 
 
-        data_list.append([curr_day, stock_code, stock_name, close_p, C.value, image, hsgt_date, hsgt_share, hsgt_percent, hsgt_delta1, hsgt_deltam, conti_day, money_total, total_mv ])
+        data_list.append([curr_day, stock_code, stock_name, close_p, C.value, image, \
+                hsgt_date, hsgt_share, hsgt_percent, hsgt_delta1, hsgt_deltam, conti_day, \
+                money_total, total_mv,\
+                is_peach, is_zig, is_quad])
 
-    data_column = ['cur_date', 'code', 'name', 'a_pct', 'close', 'image_url', 'hk_date', 'hk_share', 'hk_pct', 'hk_delta1', 'hk_deltam', 'conti_day', 'hk_m_total', 'total_mv']
+    data_column = ['cur_date', 'code', 'name', 'a_pct', 'close', 'image_url',\
+            'hk_date', 'hk_share', 'hk_pct', 'hk_delta1', 'hk_deltam', 'conti_day', \
+            'hk_m_total', 'total_mv',\
+            'peach', 'zig', 'quad']
+
     ret_df=pd.DataFrame(data_list, columns=data_column)
     ret_df['m_per_day'] = ret_df.hk_m_total / ret_df.conti_day
     ret_df = ret_df.fillna(0)
@@ -684,7 +713,12 @@ def comm_generate_web_dataframe(curr_dir, images, curr_day, dict_industry):
     if debug:
         print(ret_df)
 
-    data_column = ['cur_date', 'code', 'name', 'a_pct', 'close', 'image_url', 'hk_date', 'hk_share', 'hk_pct', 'hk_delta1', 'hk_deltam', 'conti_day', 'hk_m_total', 'm_per_day', 'total_mv']
+    data_column = ['cur_date', 'code', 'name', 'a_pct', 'close', 'image_url',\
+            'peach', 'zig', 'quad',\
+            'hk_date', 'hk_share', 'hk_pct', \
+            'hk_delta1', 'hk_deltam', 'conti_day', \
+            'hk_m_total', 'm_per_day', 'total_mv']
+
     ret_df=ret_df.loc[:,data_column]
 
     ret_df = ret_df.sort_values('hk_m_total', ascending=0)
