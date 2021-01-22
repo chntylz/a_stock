c31145651511757d886b6ac4cf90dcf9ebacee04
Your Name <you@example.com>
2021-01-22 22:45:11 +0800

    add zlje function

---
+++ eastmoney/get_daily_fund.py
@@ -0,0 +1,165 @@
+#!/#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import json
+import requests
+import re
+
+import time
+import datetime
+
+url_1 = 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112308511204703039876_1610688047489&fid=f62 &po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124'
+url_3 = 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112309724568186220448_1610691602607&fid=f267&po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf127%2Cf267%2Cf268%2Cf269%2Cf270%2Cf271%2Cf272%2Cf273%2Cf274%2Cf275%2Cf276%2Cf257%2Cf258%2Cf124'
+url_5 = 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112309724568186220448_1610691602607&fid=f164&po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf109%2Cf164%2Cf165%2Cf166%2Cf167%2Cf168%2Cf169%2Cf170%2Cf171%2Cf172%2Cf173%2Cf257%2Cf258%2Cf124'
+url_10= 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112309724568186220448_1610691602607&fid=f174&po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf160%2Cf174%2Cf175%2Cf176%2Cf177%2Cf178%2Cf179%2Cf180%2Cf181%2Cf182%2Cf183%2Cf260%2Cf261%2Cf124'
+
+mapping = {
+    "f2": "zxj",
+    "f3": "zdf",
+    "f127": "zdf",
+    "f109": "zdf",
+    "f160": "zdf",
+    "f12": "code",
+    "f14": "name",
+    "f62": "zlje",
+    "f184": "zljzb",
+    "f66": "cddje",
+    "f69": "cddjzb",
+    "f72": "ddje",
+    "f75": "ddjzb",
+    "f78": "zdje",
+    "f81": "zdjzb",
+    "f84": "xdje",
+    "f87": "xdjzb",
+    "f267": "zlje",
+    "f268": "zljzb",
+    "f269": "cddje",
+    "f270": "cddjzb",
+    "f271": "ddje",
+    "f272": "ddjzb",
+    "f273": "zdje",
+    "f274": "zdjzb",
+    "f275": "xdje",
+    "f276": "xdjzb",
+    "f164": "zlje",
+    "f165": "zljzb",
+    "f166": "cddje",
+    "f167": "cddjzb",
+    "f168": "ddje",
+    "f169": "ddjzb",
+    "f170": "zdje",
+    "f171": "zdjzb",
+    "f172": "xdje",
+    "f173": "xdjzb",
+    "f174": "zlje",
+    "f175": "zljzb",
+    "f176": "cddje",
+    "f177": "cddjzb",
+    "f178": "ddje",
+    "f179": "ddjzb",
+    "f180": "zdje",
+    "f181": "zdjzb",
+    "f182": "xdje",
+    "f183": "xdjzb",
+    "f205": "zdcode",
+    "f204": "zdname",
+    "f258": "zdcode",
+    "f257": "zdname",
+    "f261": "zdcode",
+    "f260": "zdname",
+    "f225": "zlpm1",
+    "f263": "zlpm5",
+    "f264": "zlpm10",
+    "f124": "record_date"
+};
+
+#把时间戳转化为时间: 1479264792 to 2016-11-16 10:53:12
+def get_date_from_timestamp(timestamp):
+    d = datetime.datetime.fromtimestamp(timestamp / 1000, None)  # 时间戳转换成字符串日期时间
+    my_date = d.strftime("%Y-%m-%d %H:%M:%S.%f")
+    my_date = my_date[:10]
+    return my_date
+
+
+def get_daily_fund():
+    timestamp=str(round(time.time() * 1000))
+    #url = 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112309724568186220448_1610691602607&fid=f62&po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124'
+    url = 'http://push2.eastmoney.com/api/qt/clist/get?cb=jQuery112309724568186220448_'\
+            + timestamp +'&fid=f62&po=1&pz=10000&pn=1&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=m%3A0%2Bt%3A6%2Bf%3A!2%2Cm%3A0%2Bt%3A13%2Bf%3A!2%2Cm%3A0%2Bt%3A80%2Bf%3A!2%2Cm%3A1%2Bt%3A2%2Bf%3A!2%2Cm%3A1%2Bt%3A23%2Bf%3A!2%2Cm%3A0%2Bt%3A7%2Bf%3A!2%2Cm%3A1%2Bt%3A3%2Bf%3A!2&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124'
+    print(url)
+
+   
+    response = requests.get(url)
+    p1 = re.compile(r'[(](.*?)[)]', re.S)
+    response_array = re.findall(p1, response.text)
+    api_param = json.loads(response_array[0])
+    rawdata = api_param['data']['diff']
+    data_df = pd.DataFrame(rawdata)
+    
+
+
+    return data_df
+
+def handle_raw_data():
+    nowdate=datetime.datetime.now().date()
+
+    df = get_daily_fund()
+    del df['f204']
+    del df['f205']
+    del df['f206']
+    
+    print(df)
+
+    df_list = list(df)
+    len_df = len(df_list)
+    for i in range(0, len_df):
+        print(i, len_df,df_list[i])
+        new_col_name = mapping[df_list[i]]
+        df = df.rename(columns={df_list[i]:new_col_name})
+
+    #timestamp -> date
+    df['record_date'] = nowdate.strftime("%Y-%m-%d")
+
+    #delete suspend code
+    df = df[df['zxj'] != '-']
+    df = df.reset_index(drop=True)
+
+
+    #unit: Y
+    df['zlje']  = df['zlje'].apply(lambda x: x/1000/1000/100)
+    df['cddje'] = df['cddje'].apply(lambda x: x/1000/1000/100)
+    df['ddje']  = df['ddje'].apply(lambda x: x/1000/1000/100)
+    df['zdje']  = df['zdje'].apply(lambda x: x/1000/1000/100)
+    df['xdje']  = df['xdje'].apply(lambda x: x/1000/1000/100)
+
+    df = round(df, 2)
+
+    return df
+
+if __name__ == '__main__':
+    
+    nowdate=datetime.datetime.now().date()
+    df = handle_raw_data()
+
+    print(df.loc[0])
+
+    
+'''
+zxj             139.55                  最新价
+zdf               9.19                  涨跌幅
+code            002460                  代码
+name              赣锋锂业              名字
+zlje         9.082e+08                  今日主力净流入
+cddje      1.05757e+09                  今日超大单净流入
+cddjzb            15.7                  今日超大单净流入净占比
+ddje       -1.4937e+08                  今日大单净流入
+ddjzb            -2.22                  今日大单净流入占比
+zdje      -5.21636e+08                  今日中单净流入
+zdjzb            -7.75                  今日中单净流入占比
+xdje      -3.86564e+08                  今日小单净流入
+xdjzb            -5.74                  今日小单净流入占比
+record_date     1611299103
+zljzb            13.49                  今日主力净流入占比
+'''
